e0846bb170d8516e0e4c0f52e1fc19c7e6e0e666
jextus <129642498+jextus@users.noreply.github.com>
2025-06-03 13:34:00 -0500

    Create apiClient..jsx

---
+++ kratos1.client/src/api/apiClient..jsx
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const apuClient: 'https://',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+ // 'Authorization'; 'Bearer $(localStorage.getItem('token'))'
+  }
+});
+export default apiClient;
